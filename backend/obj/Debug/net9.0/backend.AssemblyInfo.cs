488acada42014343e3bb0fa9594660ab8a00d8f9
oltianelezi <oltianelezi1@gmail.com>
2025-08-06 10:18:58 +0200

    Finished feature

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2128f8a1e6d49468f06fafe4e0c605dd6d7c46a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddeb75dcf8b8cecf4a80ec9ea0f197288d81c7d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
